90013e591ee8deeb72bedeea8961126b90be1e52
Tigran Antonyan <tigran.antonyan@gmail.com>
2015-09-30 23:22:28 -0400

    Added storm streaming -- pyleus builds. Also added rt tables

---
+++ storm/trending_rt_topology/trending_rt_topology/json-splitter_bolt.py
@@ -0,0 +1,111 @@
+from pyleus.storm import SimpleBolt
+import json
+import datetime
+from cassandra.cluster import Cluster
+
+# get poster's user
+def userId(item):
+    if ('user' in item) and isinstance(item['user'], dict):
+	if 'id_str' in item['user']:
+	    return item['user']['id_str']
+    return None
+
+# get a combined list of the hashtags and user mentions 
+def trendItems(item):
+    tags = []
+    if ('entities' in item) and isinstance(item['entities'], dict):
+	# only continue if we have a list (could be 'None')
+	if 'hashtags' in item['entities']:
+	    tags.extend(item['entities']['hashtags'])
+
+	if 'user_mentions' in item['entities']:
+	    tags.extend(item['entities']['user_mentions'])
+    return tags
+
+def tweetCountry(item):
+    if ('place' in item) and isinstance(item['place'], dict):
+        if 'country_code' in item['place']:
+	    return item['place']['country_code']
+    return None
+
+def tweetCity(item):
+    if ('place' in item) and isinstance(item['place'], dict):
+        if 'name' in item['place']:
+	    return item['place']['name']
+    return None
+
+# parse the next raw text line into a tweet -- just the needed elements
+def tweet_from_json_line(json_line):
+    try:
+        item = json.loads(json_line)
+    except:
+	return None # not a json line -- just retun None
+
+    if ('id_str' not in item) or ('timestamp_ms' not in item) or('text' not in item):
+	return None # not what we expected
+
+    tweet = {}
+    tweet['t_id'] = item['id_str']
+    tweet['u_id'] = userId(item) # just the userId is enough
+    tweet['coords'] = item['coordinates'] # list [longitude,latitude]
+    tweet['tags'] = trendItems(item) # will have the combined list of hashtags and user mentions
+    tweet['city'] = tweetCity(item)
+    tweet['country'] = tweetCountry(item) # the country code: US, UK, etc.
+    tweet['time_ms'] = long(item['timestamp_ms'])
+
+    # if any of the fields are missing return None as well
+    if ((tweet['u_id'] is None) or (tweet['city'] is None) or (tweet['country'] is None)):
+	return None
+
+    tweet['text'] = item['text'] # don't need the actual tweet...
+
+    return tweet
+
+
+cluster = Cluster(['172.31.46.91'])
+session = cluster.connect('trends')
+
+# insert into the location tables
+def insert_locations(tweet, topic):
+    cql_insert = """INSERT INTO rt_tweet_locations_world (time_ms, lat, long)
+		    VALUES (%s, %s, %s)"""
+    session.execute(cql_insert, tweet['time_ms'], float(tweet['coords'][1]), float(tweet['coords'][0]))
+    cql_insert = """INSERT INTO rt_tweet_locations_country (country, time_ms, lat, long)
+		    VALUES (%s, %s, %s, %s)"""
+    session.execute(cql_insert, tweet['country'], tweet['time_ms'], float(tweet['coords'][1]), float(tweet['coords'][0]))
+    cql_insert = """INSERT INTO rt_tweet_locations_city (country, city, time_ms, lat, long)
+		    VALUES (%s, %s, %s, %s, %s)"""
+    session.execute(cql_insert, tweet['country'], tweet['city'], tweet['time_ms'], float(tweet['coords'][1]), float(tweet['coords'][0]))
+
+def insert_tweet_text(tweet):
+    cql_insert = """INSERT INTO rt_tweet_world (topic, time_ms, tweet)
+		    VALUES (%s, %s, %s)"""
+    session.execute(cql_insert, topic, tweet['time_ms'], tweet['tweet'])
+    cql_insert = """INSERT INTO rt_tweet_city (country, city, topic, time_ms, tweet)
+		    VALUES (%s, %s, %s, %s, %s)"""
+    session.execute(cql_insert, tweet['country'], tweet['city'], topic, tweet['time_ms'], tweet['tweet'])
+    cql_insert = """INSERT INTO rt_tweet_country (country, topic, time_ms, tweet)
+		    VALUES (%s, %s, %s, %s)"""
+    session.execute(cql_insert, tweet['country'], topic, tweet['time_ms'], tweet['tweet'])
+
+
+class JsonSplitterBolt(SimpleBolt):
+
+    OUTPUT_FIELDS = ['time_ms', 'country', 'city', 'topic']
+
+    def process_tuple(self, tup):
+        tweet = tweet_from_json_line(tup.values(0))
+        if tweet is None:
+	    return
+
+	if len(tweet['coords']) > 0: # tweet with a location found
+	    insert_locations(tweet)
+
+	if len(tweet['tags']) > 0: # tweet with a topic(s) found
+	    for topic in tweet['tags']:
+		insert_tweet_text(tweet, topic)
+		# send out the general info for the tick tuple process to combine
+        	self.emit((tweet['time_ms'], tweet['country'], tweet['city'], topic), anchors=[tup])
+
+if __name__ == '__main__':
+    JsonSplitterBolt().run()
