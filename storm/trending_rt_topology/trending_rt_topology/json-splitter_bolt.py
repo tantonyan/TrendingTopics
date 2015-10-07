4508ed62c68b197bba9857d789f0a2d380b561ac
Tigran Antonyan <tigran.antonyan@gmail.com>
2015-10-07 01:36:30 -0400

    combined the two bolts and changed the cassandra inserts to batch statements

--- storm/trending_rt_topology/trending_rt_topology/json-splitter_bolt.py
+++ storm/trending_rt_topology/trending_rt_topology/json-splitter_bolt.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import time
 from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement
 
 # get poster's user
 def userId(item):
@@ -80,37 +81,108 @@ def tweet_from_json_line(json_line):
 cluster = Cluster(['172.31.46.91', '172.31.46.92', '172.31.46.93'])
 session = cluster.connect('trends')
 
-# insert into the location tables
-def insert_locations(tweet):
-    cql_insert = """INSERT INTO rt_tweet_locations_world (time_ms, lat, long)
-		    VALUES (%s, %s, %s)"""
-    session.execute(cql_insert, [tweet['time_ms'], float(tweet['coords'][1]), float(tweet['coords'][0])])
+def insert_locations(batch, tweet):
+    cql_prepare = "INSERT INTO rt_tweet_locations_world (time_ms, lat, long) VALUES (?, ?, ?)"
+    loc_stmt = session.prepare(cql_prepare)
 
-def insert_tweet_text(tweet, topic):
-    cql_insert = """INSERT INTO rt_tweet_world (topic, user, time_ms, tweet)
-		    VALUES (%s, %s, %s, %s)"""
-    session.execute(cql_insert, [topic, tweet['userName'], tweet['time_ms'], tweet['text']])
+    batch.add(cql_prepare, [tweet['time_ms'], float(tweet['coords'][1]), float(tweet['coords'][0])])
+
+def insert_tweet_text(batch, tweet, topic):
+    cql_prepare = "INSERT INTO rt_tweet_world (topic, user, time_ms, tweet) VALUES (?, ?, ?, ?)"
+    tweet_stmt = session.prepare(cql_prepare)
+
+    batch.add(cql_prepare, [topic, tweet['userName'], tweet['time_ms'], tweet['text']])
+
+def batch_trends_world(batch, minuteslot, records): # records is an array of (topic, count)
+    cql_prepare = "INSERT INTO world_minute_trends (minuteslot, topic, count) VALUES (?, ?, ?)"
+    world_stmt = session.prepare(cql_prepare)
+
+    for topic, count in records.iteritems():
+      batch.add(world_stmt, [minuteslot, topic, count])
+
+def batch_trends_country(batch, minuteslot, records): # records is an array of (country, topic, count)
+    cql_prepare = "INSERT INTO country_minute_trends (minuteslot, country, topic, count) VALUES (?, ?, ?, ?)"
+    country_stmt = session.prepare(cql_prepare)
+
+    for record, count in records.iteritems():
+      batch.add(country_stmt, [minuteslot, record[0], record[1], count])
+
+def batch_trends_city(batch, minuteslot, records): # records is an array of (country, city, topic, count)
+    cql_prepare = "INSERT INTO city_minute_trends (minuteslot, country, city, topic, count) VALUES (?, ?, ?, ?, ?)"
+    city_stmt = session.prepare(cql_prepare)
+
+    for record, count in records.iteritems():
+      batch.add(city_stmt, [minuteslot, record[0], record[1], record[2], count])
+
 
 class JsonSplitterBolt(SimpleBolt):
 
-    OUTPUT_FIELDS = ['time_ms', 'country', 'city', 'topic']
+#    OUTPUT_FIELDS = ['time_ms', 'country', 'city', 'topic']
+    def initialize(self):
+	self.worldTrends = {}
+	self.countryTrends = {}
+	self.cityTrends = {}
+	self.minuteslot = long(time.strftime('%Y%m%d%H%M'))
+	self.stmtCounter = 0
+	self.batchS = BatchStatement() # frequent batch
+	self.batchM = BatchStatement() # minute batch
+
+    # save/make the minute based dictionaries
+    def build_minute_items(self, country, city, topic):
+	cityKey = (country, city, topic)
+	countryKey = (country, topic)
+	worldKey = topic
+
+	if cityKey in self.cityTrends:
+	    self.cityTrends[cityKey] += 1
+	else:
+	    self.cityTrends[cityKey] = 1
+
+	if countryKey in self.countryTrends:
+	    self.countryTrends[countryKey] += 1
+	else:
+	    self.countryTrends[countryKey] = 1
+
+	if worldKey in self.worldTrends:
+	    self.worldTrends[worldKey] += 1
+	else:
+	    self.worldTrends[worldKey] = 1
 
     def process_tuple(self, tup):
         json_tweet, = tup.values
-#	self.emit(['From Bolt', str(len(json_tweet)), str(type(json_tweet)), '---------------------'])
         tweet = tweet_from_json_line(json_tweet)
         if (tweet is None) or (tweet['time_ms'] + 2000 < (time.time() * 1000L)): # only recent (within last 2 secnds) tweets
-#	    self.emit(['From Bolt', "--- --- None --- ---", " --- --- parsing error ---", '---------------------'])
 	    return
 
 	if len(tweet['coords']) > 0: # tweet with a location found
-	    insert_locations(tweet)
+	    insert_locations(self.batchS, tweet)
+	    self.stmtCounter += 1
+	    if (self.stmtCounter == 20):
+		session.execute(self.batchS)
+		self.stmtCounter = 0
 
 	if len(tweet['tags']) > 0: # tweet with a topic(s) found
 	    for topic in tweet['tags']:
-		insert_tweet_text(tweet, topic)
-		# send out the general info for the tick tuple process to combine
-        	self.emit((tweet['time_ms'], tweet['country'], tweet['city'], topic), anchors=[tup])
+		insert_tweet_text(self.batchS, tweet, topic)
+	        self.stmtCounter += 1
+	        if (self.stmtCounter == 20):
+		    session.execute(self.batchS)
+		    self.stmtCounter = 0
+
+		self.build_minute_items(tweet['country'], tweet['city'], topic)
+#        	self.emit((tweet['time_ms'], tweet['country'], tweet['city'], topic), anchors=[tup])
+
+    def process_tick(self):
+	self.minuteslot = long(time.strftime('%Y%m%d%H%M'))
+
+	batch_trends_city(self.batchM, self.minuteslot, self.cityTrends)
+	batch_trends_country(self.batchM, self.minuteslot, self.countryTrends)
+	batch_trends_world(self.batchM, self.minuteslot, self.worldTrends)
+
+	session.execute(self.batchM)
+	self.worldTrends.clear()
+	self.countryTrends.clear()
+	self.cityTrends.clear()
 
 if __name__ == '__main__':
     JsonSplitterBolt().run()
